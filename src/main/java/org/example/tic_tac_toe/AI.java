ebcec4fb8534dd075cece43423719f9a942d5b97
Polina Kolesnikova <polina@example.com>
2024-01-24 08:48:43 +0100

    check

--- src/main/java/org/example/tic_tac_toe/AI.java
+++ src/main/java/org/example/tic_tac_toe/AI.java
@@ -126,49 +126,5 @@ public class AI {
         }
 
 
-//        if (Field.getAiDots().size() == 0) {
-//            int x, y;
-//            do {
-//                x = random.nextInt(field.getSize());
-//                y = random.nextInt(field.getSize());
-//            } while (!field.isCellValid(x, y));
-//            field.setDot(x, y, DOT);
-//        }else{
-//
-//            HashMap<int[], Integer> dotsIndex = Field.getDotsIndex();
-//
-//            int x = 0, y = 0;
-//            do{
-//
-//            for (Line line: lines
-//                 ) {
-//                int[] array = line.getArray();
-//                for (int i = 0; i < array.length; i++) {
-//                    for (int j = 0; j < Field.getAiDots().size(); j++) {
-//                        if(array[i]==Field.getAiDots().get(j)){
-//                            int iTemp;
-//                            do{
-//                                 iTemp = random.nextInt(0,3);
-//                            }while(iTemp!=i);
-//
-//                            for(int[] coord: dotsIndex.keySet()){
-//                                if(dotsIndex.get(coord)==iTemp){
-//                                    x = coord[0];
-//                                    y = coord[1];
-//                                }
-//                            }
-//                        }
-//                    }
-//
-//                }
-//
-//            }
-//
-//                field.setDot(x, y, DOT);
-//            }while(!field.isCellValid(x, y));
-//
-//        }
-//    }
-
 
 
